3f19798970384ada7202d8557e0ab81538e8ea3d
Josquin Larsen <jsqnlrsn@gmail.com>
2026-01-14 18:33:59 -0500

    setup backend files, create models

---
+++ backend/models.py
@@ -0,0 +1,37 @@
+from sqlalchemy import (
+    Column, 
+    ForeignKey,
+    String,
+)
+
+from sqlalchemy.orm import DeclarativeBase, relationship
+
+class Base(DeclarativeBase):
+    pass
+
+class User(DeclarativeBase):
+    """
+    User table in database
+    """
+
+    __tablename__ = "user"
+    id = Column(String, primary_key=True)
+    email = Column(String, unique=True, nullable=False)
+    password = Column(String, nullable=False)
+
+class Account(DeclarativeBase):
+    """
+    Account Table in database
+    """
+
+    __tablename__ = "account"
+    id = Column(String, primary_key=True)
+    username = Column(String, nullable=False)
+    password = Column(String, nullable=False)
+    site = Column(String, nullable=False)
+    notes = Column(String, nullable=True)
+    encryption_key = Column(String, nullable=False) # maybe set to True for beginning?
+    user_id = Column(String, ForeignKey("user.id"))
+    user = relationship("User", backref="account")
+
+
