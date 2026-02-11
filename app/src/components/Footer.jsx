ea9dacb2031bf754b471c0731d33039a8dd959d5
Josquin Larsen <jsqnlrsn@gmail.com>
2026-02-11 13:06:45 -0500

    fix long lines in backend; start navbar, footer, searchbar

---
+++ app/src/components/Footer.jsx
@@ -0,0 +1,12 @@
+import '../App.css'
+
+export default function Footer() {
+    const year = new Date().getFullYear();
+    return (
+        <>
+        <footer>
+            <p>&copy;{year}</p>
+        </footer>
+        </>
+    )
+}
