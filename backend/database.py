fa8c017d1ede24e864433135bde26491e06c80ce
Josquin Larsen <jsqnlrsn@gmail.com>
2026-02-16 20:45:16 -0500

    created docker-compose for local development

--- backend/database.py
+++ backend/database.py
@@ -1,12 +1,18 @@
 from sqlalchemy import create_engine
 from sqlalchemy.orm import sessionmaker
 
-SQLALCHEMY_DATABASE_URL = "sqlite:///./spm.db"
+import os
 
-# check same thread may need to change for Postgres
-engine = create_engine(
-    SQLALCHEMY_DATABASE_URL, connect_args={"check_same_thread": False}
-)
+POSTGRES_USER = os.environ["POSTGRES_USER"]
+POSTGRES_DB = os.environ.get("POSTGRES_DB", POSTGRES_USER)
+POSTGRES_HOST = os.environ.get("POSTGRES_HOST", "localhost")
+POSTGRES_PASSWORD = os.environ.get("POSTGRES_PASSWORD", "")
+POSTGRES_ENGINE = os.environ.get("POSTGRES_ENGINE", "postgresql+psycopg2")
+
+SQLALCHEMY_DATABASE_URL = "sqlite:///./spm.db"
+POSTGRES_DB_URL = f"{POSTGRES_ENGINE}://{POSTGRES_USER}:{POSTGRES_PASSWORD}@{POSTGRES_HOST}:5432/spm_db"
+
+engine = create_engine(POSTGRES_DB_URL)
 
 LocalSession = sessionmaker(autocommit=False, autoflush=False, bind=engine)
 
