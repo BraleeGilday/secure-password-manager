eeeeaf98dc95d0dc9cd2f62cd96d2cd2dcd13dae
Josquin Larsen <jsqnlrsn@gmail.com>
2026-02-23 12:25:20 -0500

    clean up and test scripts to load docker to ec2

--- deploy.sh
+++ deploy.sh
@@ -2,19 +2,21 @@
 #!/bin/bash
 set -e
 
+set -a
 source .env.prod
-
-echo timeout: "$TIMEOUT"
+set +a
 
 echo "creating docker images"
 
-docker compose -f docker-compose.prod.yml build frontend 
+docker compose -f docker-compose.prod.yml build  
 docker image prune -f
-docker save -o spm_build.tar frontend:latest
+docker save -o spm_build.tar spm-backend:latest spm-frontend:latest
 
 echo "copying over docker images"
 
-# scp -i "$PEM_PATH" spm_build.tar "$EC2_USER@$EC2_HOST"
+scp -i "$PEM_PATH" spm_build.tar "$EC2_USER@$EC2_HOST":~/
+
+echo "clean up!"
 
 rm -vf spm_build.tar
 
--- deploy_ec2.sh
+++ deploy_ec2.sh
@@ -1,10 +1,32 @@
 # for use in EC2 instance CLI
 #!/bin/bash
+set -e
 
-docker load -i spm_build.tar
+set -a
+source .env.prod
+set +a
 
-rm -vf spm_build.tar
+echo "logging into AWS EC2"
+ssh -i "$PEM_PATH" "$EC2_USER@$EC2_HOST" << EOF
+    # make sure we actually log in...
+    sleep 5 &
 
-docker image prune -f
+    echo "loading images"
+    docker load -i spm_build.tar
 
-echo "run 'docker compose -f docker-compose.prod.yml' up to start server"
+    cd secure-password-manager
+    echo "current directory: \$(pwd)"
+
+    echo "starting server..."
+    docker compose -f docker-compose.prod.yml up -d --remove-orphans
+
+    echo "cleaning up..."
+    rm -vf ~/spm_build.tar
+
+    docker image prune -f
+EOF
+
+echo "job's done!"
+echo "you can now view SPM"
+
+# echo "run 'docker compose -f docker-compose.prod.yml' up to start server"
