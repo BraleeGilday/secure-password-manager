c5f4349d8315dc0aeecf90f0e85ace715f1ac39d
Josquin Larsen <jsqnlrsn@gmail.com>
2026-02-25 12:49:41 -0500

    toggle to AWS for api/client

--- app/src/api/client.js
+++ app/src/api/client.js
@@ -1,13 +1,13 @@
 import axios from "axios";
 
 // local
-const api = axios.create({
-  baseURL: "http://127.0.0.1:8000",
-});
+// const api = axios.create({
+//   baseURL: "http://127.0.0.1:8000",
+// });
 
 // deployment test
-// const api = axios.create({
-//   baseURL: "",
-// });
+const api = axios.create({
+  baseURL: "",
+});
 
 export default api;
