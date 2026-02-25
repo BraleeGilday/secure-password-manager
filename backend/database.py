4a30af059bafa0b8ed00f5ffbf50335ae3e4fcec
Josquin Larsen <jsqnlrsn@gmail.com>
2026-02-25 12:45:10 -0500

    database: toggle between env and prod vars

--- backend/database.py
+++ backend/database.py
@@ -4,20 +4,34 @@ import os
 from dotenv import load_dotenv
 
 load_dotenv()
+ENV = os.getenv("APP_ENV", "development")
 
-USER = os.environ["POSTGRES_USER"]
-PWD = os.environ.get("POSTGRES_PASSWORD", "default")
-DB = os.environ.get("POSTGRES_DB", "spm_db")
-HOST = os.environ.get("POSTGRES_HOST", "localhost")
-PORT = os.environ.get("POSTGRES_PORT", "5432")
-ENGINE = os.environ.get("POSTGRES_ENGINE", "postgresql+psycopg2")
+if ENV == "development":
+    USER = os.environ["POSTGRES_USER"]
+    PWD = os.environ.get("POSTGRES_PASSWORD", "default")
+    HOST = os.environ.get("POSTGRES_HOST", "localhost")
+    PORT = os.environ.get("POSTGRES_PORT", "5432")
+    DB = os.environ.get("POSTGRES_DB", "spm_db")
+    ENGINE = os.environ.get("POSTGRES_ENGINE", "postgresql+psycopg2")
+
+    connect_args = {}
+
+else:
+    USER = os.environ["RDS_USER"]
+    PWD = os.environ.get("RDS_PWD")
+    HOST = os.environ.get("RDS_HOST")
+    PORT = os.environ.get("RDS_PORT", "5432")
+    DB = os.environ.get("POSTGRES_DB")
+    ENGINE = os.environ.get("POSTGRES_ENGINE", "postgresql+psycopg2")
+
+    connect_args = {"sslmode": "require"}
 
 SQLALCHEMY_DATABASE_URL = "sqlite:///./spm.db"
 POSTGRES_DB_URL = (
     f"{ENGINE}://{USER}:{PWD}@{HOST}:{PORT}/{DB}"
 )
 
-engine = create_engine(POSTGRES_DB_URL)
+engine = create_engine(POSTGRES_DB_URL, connect_args=connect_args)
 
 LocalSession = sessionmaker(autocommit=False, autoflush=False, bind=engine)
 
