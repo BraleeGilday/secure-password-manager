a747d1c7c6883a79bc31b1ca5fe988185178dd5a
Josquin Larsen <jsqnlrsn@gmail.com>
2026-03-03 15:18:09 -0500

    remove dark mode, fix logout button, change search bar color to magenta, remove pwd gen from welcome

--- app/src/pages/Welcome.jsx
+++ app/src/pages/Welcome.jsx
@@ -1,18 +1,12 @@
 import Card from "../components/Card";
 
-import PasswordGeneratorButton from "./pwd_gen/PasswordGeneratorButton";
-
 export default function Welcome() {
-    // pwd gen modal will need to be relocated to appropriate page (credentials)
-
 
     let welcomeText = "Welcome to Secure Password Manager (SPM)! New users: click 'register' to get started."
 
     return(
         <div className="page-center">
             <Card content={welcomeText} />
-            
-            <PasswordGeneratorButton />
         </div>
     )
 }
