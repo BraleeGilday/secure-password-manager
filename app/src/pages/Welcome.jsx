5320ef4d1e78df8ded0d3bb60b4818e1c8af04f6
Josquin Larsen <jsqnlrsn@gmail.com>
2026-03-03 19:51:44 -0500

    update welcome page layout

--- app/src/pages/Welcome.jsx
+++ app/src/pages/Welcome.jsx
@@ -2,16 +2,12 @@ import Card from "../components/Card";
 
 export default function Welcome() {
 
-<<<<<<< HEAD
     let welcomeText = (
         <div style={{textAlign: "center", padding: "2px"}}>
             <p>Welcome to Secure Password Manager (SPM)!</p>
             <p>New users: click <strong>register</strong> to get started."</p>
         </div>
         )
-=======
-    let welcomeText = "Welcome to Secure Password Manager (SPM)! New users: click 'register' to get started."
->>>>>>> a747d1c (remove dark mode, fix logout button, change search bar color to magenta, remove pwd gen from welcome)
 
     return(
         <div className="page-center">
