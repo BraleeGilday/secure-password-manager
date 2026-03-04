07c9f66dd56943bd35e73d05c6109b1a81e4114b
Josquin Larsen <jsqnlrsn@gmail.com>
2026-03-04 11:51:38 -0500

    update welcome message and card border

--- app/src/pages/Welcome.jsx
+++ app/src/pages/Welcome.jsx
@@ -5,7 +5,8 @@ export default function Welcome() {
     let welcomeText = (
         <div style={{textAlign: "center", padding: "2px"}}>
             <p>Welcome to Secure Password Manager (SPM)!</p>
-            <p>New users: click <strong>register</strong> to get started."</p>
+            <p>Safely store your credentials in one place.</p>
+            <p>New users: click <strong>register</strong> to get started.</p>
         </div>
         )
 
